480a0918147f74809b7be70ad5cc00854fa20eb9
Mondher Essid <me@vyzyo.com>
2025-02-18 21:33:33 +0100

    AppBar

--- app/src/main/java/com/creative/weather_app/ui/screen/main/MainScreen.kt
+++ app/src/main/java/com/creative/weather_app/ui/screen/main/MainScreen.kt
@@ -1,25 +1,33 @@
 package com.creative.weather_app.ui.screen.main
 
+import android.annotation.SuppressLint
+import android.util.Log
 import androidx.compose.foundation.layout.Box
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.creative.weather_app.data.DataofException
 import com.creative.weather_app.model.weatherResponse
+import com.creative.weather_app.widgets.weatherAppBar
 
 @Composable
 fun MainScreen(navController: NavHostController, mainViewModel: MainScreenViewModel) {
 
 
-    get_Weather(mainViewModel)
+    get_Weather(mainViewModel, navController)
 
 }
 
 @Composable
-private fun get_Weather(mainViewModel: MainScreenViewModel) {
+private fun get_Weather(mainViewModel: MainScreenViewModel, navController: NavHostController) {
     val weatherData = produceState<DataofException<weatherResponse, Boolean, Exception>>(
         initialValue = DataofException(loading = true)
     ) {
@@ -30,7 +38,33 @@ private fun get_Weather(mainViewModel: MainScreenViewModel) {
             CircularProgressIndicator()
         }
     } else if (weatherData.data != null) {
-        Text(text = "Main Screen ${weatherData.data.toString()}")
+        MainScaffold(navController, weatherData.data!!)
+    }
+}
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun MainScaffold(mainViewModel: NavHostController, data: weatherResponse) {
+    Scaffold(topBar = {
+        weatherAppBar(
+            data.city.name + "," + data.city.country,
+            elevation = 5.dp,
+            navController = mainViewModel,
+            icon = Icons.Default.ArrowBack
+        )
+        {
+            Log.d("mondher","buttonClicked")
+        }
+    })
+    {
+        MainContent(data)
 
     }
+
+}
+
+@Composable
+fun MainContent(data: weatherResponse) {
+    Text(text = data.city.name)
+
 }
---
+++ app/src/main/java/com/creative/weather_app/widgets/weatherAppBar.kt
@@ -0,0 +1,78 @@
+package com.creative.weather_app.widgets
+
+import androidx.collection.emptyLongSet
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun weatherAppBar(
+    tile: String,
+    icon: ImageVector? = null,
+    isMainScreen: Boolean = true,
+    elevation: Dp = 0.dp,
+    navController: NavController,
+    onAddActionClicked: () -> Unit = {},
+    onButtonClicked: () -> Unit = {}
+) {
+
+    TopAppBar(
+        title = { Text(text = tile) },
+        modifier = Modifier.shadow(elevation = elevation),
+        actions = {
+            if (isMainScreen) {
+                IconButton(onClick = {}) {
+                    Icon(
+                        imageVector = Icons.Default.Search,
+                        contentDescription = "search icon",
+                    )
+                }
+
+                IconButton(onClick = {}) {
+                    Icon(
+                        imageVector = Icons.Default.MoreVert,
+                        contentDescription = "More icon",
+                    )
+                }
+            } else {
+                Box(modifier = Modifier.padding(0.dp)) { }
+            }
+
+
+        },
+        navigationIcon = {
+            if (icon != null) {
+                Icon(
+                    imageVector = icon,
+                    contentDescription = null,
+                    modifier = Modifier.clickable {
+                        onButtonClicked.invoke()
+                    })
+            }
+
+
+        },
+        colors = topAppBarColors(Color.White)
+    )
+
+
+}
